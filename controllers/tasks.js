00f4addbc92832a7e59e4d297487137f5131fe0f
G00syara <sasha.ugarov.02@mail.ru>
2023-04-21 12:25:17 +0300

    Complete cotrollers, check their work in postman, back is over on 90%

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -31,11 +31,37 @@ const getTask = async (req, res) => {
     res.status(500).json({ msg: error })
   }
 }
-const updateTask = (req, res) => {
-  res.send('update task')
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params
+
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    })
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` })
+    }
+
+    res.status(200).json({ task })
+    //res.status(200).json({ id: taskID, data: req.body }) Проверка обновляет ли вообще
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
-const deleteTask = (req, res) => {
-  res.send('delete task')
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params
+    const task = await Task.findOneAndDelete({ _id: taskID })
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` })
+    }
+    res.status(200).json({ task })
+    //res.status(200).send()
+    //res.status(200).json({task:null, status: 'success'})
+  } catch (error) {
+    res.status(500).json({ msg: error })
+  }
 }
 
 module.exports = {

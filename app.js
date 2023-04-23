b7a81d29cd5b4704f5686bf703995ad6a7aab2d9
G00syara <sasha.ugarov.02@mail.ru>
2023-04-24 00:21:02 +0300

    make comments for my code

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express') //Библиотека, для загрузки файлов и модулей
 const app = express()
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
@@ -7,8 +7,8 @@ const notFound = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
 //middleware
-app.use(express.static('./public'))
-app.use(express.json())
+app.use(express.static('./public')) //Соединяет Бэк с Фронтом
+app.use(express.json()) //use промежуточное ПО
 
 //routes
 // app.get('/', (req, res) => {
